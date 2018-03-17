8110266e6624296aad432997bb50369e3c514263
Mikhail Polyevin <mpolyevin@gmail.com>
2018-03-17 16:55:56 +0300

    Initial Commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Mikhail Polyevin on 17/03/2018.
+//  Copyright © 2018 MP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
